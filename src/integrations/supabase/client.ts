834e9869d6a702daeeec413ac0b14c57c6614599
vhervatin <54643926+vhervatin@users.noreply.github.com>
2025-04-29 00:00:32 -0300

    config base

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -4,7 +4,7 @@ import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
 const SUPABASE_URL = "https://bbotlgrvdehuhmiletdf.supabase.co";
-const SUPABASE_KEY = "eeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJib3RsZ3J2ZGVodWhtaWxldGRmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU4NjI1NDksImV4cCI6MjA2MTQzODU0OX0.MAdQLvNxyh4920AAQQ2uaTyERWoDwhvr2HlM0Y3slrM";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJib3RsZ3J2ZGVodWhtaWxldGRmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU4NjI1NDksImV4cCI6MjA2MTQzODU0OX0.MAdQLvNxyh4920AAQQ2uaTyERWoDwhvr2HlM0Y3slrM";
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
